59dac0eae48e78db9bb943d34c57bc11d3408676
jrkapciak <jerzyryszardkapciak@gmail.com>
2017-11-20 13:01:52 +0100

    updated views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,9 +4,13 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from . import serializers
 from . import models
+from . import permissions
+
 # Create your views here.
 
 class HelloApiView(APIView):
@@ -106,6 +110,13 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
+
+
+
 
 
 
